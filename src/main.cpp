2726534bf3404ba70f209baee29567f427ada9f6
Viraj Sinha <virajosinha@gmail.com>
2017-06-03 16:15:03 -0700

    writes colors and makes stars

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,30 @@ int numColumns = 144;
 
 uint32_t white = strip.Color(255, 255, 255);
 uint32_t red = strip.Color(255, 0, 0);
+uint32_t green = strip.Color(0, 255, 0);
 
 struct Point {
     uint8_t  x;
     uint8_t  y;
 };
+
+uint32_t oppositeColor(uint32_t color) {
+    return ~color;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    } else if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    } else {
+        WheelPos -= 170;
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    }
+}
 
 void writePixel(byte row, byte col, uint32_t color) {
     int index = 0;
@@ -36,11 +55,53 @@ void writePixel(byte row, byte col, uint32_t color) {
     strip.setPixelColor(index, color);
 }
 
-Point points[10] = {};
+Point points[5] = {};
 uint numPoints = sizeof(points) / sizeof(Point);
 
 int columnTick = 0;
 bool wipeForward = true;
+
+byte colorTick = 0;
+uint32_t color = strip.Color(255, 0, 0);
+
+// Returns the Red component of a 32-bit color
+uint8_t Red(uint32_t color)
+{
+    return (color >> 16) & 0xFF;
+}
+
+// Returns the Green component of a 32-bit color
+uint8_t Green(uint32_t color)
+{
+    return (color >> 8) & 0xFF;
+}
+
+// Returns the Blue component of a 32-bit color
+uint8_t Blue(uint32_t color)
+{
+    return color & 0xFF;
+}
+
+uint32_t advanceColor() {
+    colorTick += 40;
+    return Wheel(colorTick);
+}
+
+uint32_t whitenColor(uint32_t color, int n) {
+    n = n * 5;
+
+    byte r = Red(color);
+    r = max(r, r + n);
+
+    byte g = Green(color);
+    g = max(g, g + n);
+
+    byte b = Blue(color);
+    b = max(b, b + n);
+
+    return strip.Color(r, g, b);
+}
+
 void columnWipe() {
 
     if (!wipeForward) {
@@ -48,7 +109,8 @@ void columnWipe() {
     }
 
     for (int r = 0; r < numRows; r++) {
-        writePixel(r, columnTick, strip.Color(25*r, 25*r, 111+columnTick));
+        //uint32_t grad = whitenColor(color, r);
+        writePixel(r, columnTick, color);
     }
 
     if (wipeForward) {
@@ -57,20 +119,34 @@ void columnWipe() {
 
     if (columnTick >= numColumns || columnTick <= 0) {
         wipeForward = !wipeForward;
+
+        color = advanceColor();
     }
 }
 
 void randomizePoints() {
-    for (uint i = 0; i < numPoints; i++) {
-        uint8_t randRow = random(10);
-        uint8_t randCol = random(144);
-        points[i] = {randRow, randCol};
+
+    if (wipeForward) {
+        // [ 0, columnTick ]
+        for (uint i = 0; i < numPoints; i++) {
+            uint8_t randRow = random(10);
+            uint8_t randCol = random(columnTick);
+            points[i] = {randRow, randCol};
+        }
+
+    } else {
+        // [ columnTick, numColumns ]
+        for (uint i = 0; i < numPoints; i++) {
+            uint8_t randRow = random(10);
+            uint8_t randCol = columnTick + random(144 - columnTick);
+            points[i] = {randRow, randCol};
+        }
     }
 }
 
 void drawPoints() {
     for (uint i = 0; i < numPoints; i++) {
-        writePixel(points[i].x, points[i].y, red);
+        writePixel(points[i].x, points[i].y, oppositeColor(color));
     }
 }
 
@@ -81,7 +157,7 @@ void setup()
     randomSeed(analogRead(0));
 
     strip.begin();
-    strip.setBrightness(10);
+    strip.setBrightness(50);
 
     for (uint i = 0; i < numPoints; i++) {
         points[i] = {0, 0};
